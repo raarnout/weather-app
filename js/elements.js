f76c0fcaa884e67f61770291c61fc67215085bd0
raarnout <ronaldaarnoutse@gmail.com>
2021-01-17 20:58:39 +0100

    format elements module

--- js/elements.js
+++ js/elements.js
@@ -1,9 +1,13 @@
-export const ELEMENT_SEARCH_BUTTON = document.querySelector('button');
-export const ELEMENT_SEARCH_CITY = document.querySelector('#city');
+export const ELEMENT_SEARCH_BUTTON = document.querySelector("button");
+export const ELEMENT_SEARCH_CITY = document.querySelector("#city");
 
-export const ELEMENT_LOADING_TEXT = document.querySelector('#load');
-export const ELEMENT_WEATHER_CONTAINER= document.querySelector('#weather');
+export const ELEMENT_LOADING_TEXT = document.querySelector("#load");
+export const ELEMENT_WEATHER_CONTAINER = document.querySelector("#weather");
 
-export const ELEMENT_WEATHER_CITY= ELEMENT_WEATHER_CONTAINER.firstElementChild;
-export const ELEMENT_WEATHER_DESCRIPTION= document.querySelector('#weatherDescription');
-export const ELEMENT_WEATHER_TEMPERATURE= ELEMENT_WEATHER_CONTAINER.lastElementChild;
+export const ELEMENT_WEATHER_CITY = ELEMENT_WEATHER_CONTAINER.firstElementChild;
+export const ELEMENT_WEATHER_DESCRIPTION = document.querySelector(
+	"#weatherDescription"
+);
+export const ELEMENT_WEATHER_TEMPERATURE =
+    ELEMENT_WEATHER_CONTAINER.lastElementChild;
+    
