b0c90a7dff0a0c6cf35f6762eb955919c3c53bda
raarnout <ronaldaarnoutse@gmail.com>
2021-01-17 18:02:19 +0100

    added weather module

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-import * as ELEMENTS from './elements.js';
-import { search as searchWeather } from './weather.js';
+import * as ELEMENTS from "./elements.js";
+import { search as searchWeather } from "./weather.js";
 
-ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
+ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener(
+	"click",
+	searchWeather.bind(this, ELEMENTS.ELEMENT_SEARCH_CITY)
+);
--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,10 @@
-const search = () => {
-    alert('search!')
-}
+const search = (CITY_ELEMENT, event) => {
+	const CITY_NAME = CITY_ELEMENT.value.trim();
 
-export {
-    search
-}
+	if (CITY_NAME.length === 0) {
+		return alert("Please enter a city name.");
+	}
+	alert(CITY_NAME);
+};
+
+export { search };
